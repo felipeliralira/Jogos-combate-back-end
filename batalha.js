7ce80b1aabeb8028c3adb5bbb4d248439e336371
felipeliralira <felipe.lira.oliveira@escola.pr.gov.br>
2026-05-18 09:09:20 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -1,2 +1,13 @@
- 
- 
+
+function lutar(){
+    const classe = document.getElementById('classe').value;
+    const arma = document.getElementById('arma');
+    const log = document.getElementById('log');
+
+    if (!arma){
+        log.innerHTML = 'Por favor, selecione uma arma para lutar!';
+        return;
+    }
+    const dado = Math.floor(Math.random() * 20) + 1;
+    let resultado = `Dado: ${dado} | Classe: <strong>${classe}</strong> | Arma: <strong>${arma}</strong>`;
+}
